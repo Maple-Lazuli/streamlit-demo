0ac2964c74ed1d596f2d041f885a300af7736e7c
Ada <the.nostra.tymus@gmail.com>
2024-11-14 14:01:57 -0500

    added a pruning function and removed a debug print

--- src/data_generator.py
+++ src/data_generator.py
@@ -3,25 +3,23 @@ import random
 
 
 def generate_team(team_size: int, num_malicious: int = 1, accidental_exploration: int = .05,
-                  intentional_exploration: int = .25,
+                  intentional_exploration: int = .15,
                   person_base_name: str = "Zoo_Keeper") -> dict:
     team = dict()
     for i in range(team_size):
-        team[f'{person_base_name}_{i}'] = {'legitimate': True}
-        team[f'{person_base_name}_{i}'] = {'threshold': accidental_exploration * 100}
+        team[f'{person_base_name}_{i}'] = {'threshold': accidental_exploration * 100, 'legitimate': True}
 
     for i in range(num_malicious):
-        malicious_person = team.keys()[random.randint(0, team_size - 1)]
-        team[malicious_person] = {'legitimate': False}
-        team[malicious_person] = {'threshold': intentional_exploration * 100}
+        malicious_person = [_ for _ in team.keys()][random.randint(0, team_size - 1)]
+        team[malicious_person] = {'threshold': intentional_exploration * 100, 'legitimate': False}
 
     return team
 
 
-def generate_interactions(team: dict, num_team_animals: int = 15, num_days: int = 7, daily_step_size: int = 5,
-                          interactions_per_step: int = 5) -> list:
+def generate_interactions(team: dict, num_team_animals: int = 15, num_days: int = 7, daily_step_size: int = 5) -> list:
     week_start = datetime.strptime("01-01-2024 08:00:00", "%d-%m-%Y %H:%M:%S")
     interactions = []
+    interactions_per_step = int(len([_ for _ in team.keys()]) / 4)
 
     with open("../data/animals.txt", "r") as file_in:
         animals = file_in.readlines()
@@ -39,7 +37,7 @@ def generate_interactions(team: dict, num_team_animals: int = 15, num_days: int 
         while current_day_time < current_day_end_time:
             interactions_step = random.randint(0, interactions_per_step)
             for _ in range(interactions_step):
-                zoo_keeper = team.keys()[random.randint(0, len(team.keys()))]
+                zoo_keeper = [_ for _ in team.keys()][random.randint(0, len(team.keys()) - 1)]
                 selected_zoo_keeper = team[zoo_keeper]
                 event = random.randint(0, 100)
 
@@ -51,4 +49,16 @@ def generate_interactions(team: dict, num_team_animals: int = 15, num_days: int 
 
                 interactions.append((zoo_keeper, random_animal, current_day_time, selected_zoo_keeper['legitimate']))
             current_day_time = current_day_time + timedelta(minutes=1)
-    return interactions # maybe make this a generator instead
+    return interactions  # maybe make this a generator instead
+
+
+def prune_interactions(interactions):
+    seen_interactions = set()
+    pruned_interactions = []
+    for interaction in interactions:
+        event = f'{interaction[0]}-{interaction[1]}'
+        if event in seen_interactions:
+            continue
+        seen_interactions.add(event)
+        pruned_interactions.append(interaction)
+    return interactions
